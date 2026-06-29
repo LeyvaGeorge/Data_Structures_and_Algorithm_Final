cccdfa37baf67a59a8185f10cd54484b26697ea1
George Leyva <leyvageorge@hotmail.com>
2026-06-28 18:45:20 -0700

    Person class template

--- Person.java
+++ Person.java
@@ -1,3 +1,34 @@
 public class Person {
-        
+    private String fName;
+    private String lName;
+    private int age;
+    
+    //Constructor
+    public Person(String firstName, String lastName, int years) {
+        this.fName = firstName;
+        this.lName = lastName;
+        this.age = years;
+    }
+
+    //Getters and Setters
+    public String getFirstName() {
+        return this.fName;
+    }
+    public void setFirstName(String newName) {
+        this.fName = newName;
+    }
+
+    public String getLastName() {
+        return this.lName;
+    }
+    public void setLastName(String newName) {
+        this.lName = newName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+    public void setAge(int newAge) {
+        this.age = newAge;
+    }
 }
