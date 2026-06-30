67300e7b8c559c8fd02eb1e94b253e89930d4736
George Leyva <leyvageorge@hotmail.com>
2026-06-29 21:46:33 -0700

    added ability to add a person to queue class

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Queue room = new Queue();
+        room.insertPerson("James","Smith", 25);
+        room.insertPerson("Bethany", "Dove", 21);
+        room.printAll();
     }
 }
--- Queue.java
+++ Queue.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
 public class Queue{
+    ArrayList<Person> people = new ArrayList<Person>();
     
+    public Queue() {
+    }
+    public void insertPerson(String first, String last, int age){
+        Person body = new Person(first,last, age);
+        people.add(body);
+        return;
+    }
+    public void printAll() {
+        for (Person body : people){
+            System.out.println(body.getFirstName());
+            System.out.println(body.getLastName());
+            System.out.println(body.getAge());
+        }
+    }
+    public void quickSort(){
+
+    }
 }
